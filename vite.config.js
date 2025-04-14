92697abfb7e8f6d64dcf24a7dbce178ebb7629df
Siddhant Shukla <shuklasiddhant365@gmail.com>
2025-04-15 01:16:38 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/114/', // <-- Add this line with your repo name
+  base: '/DeVyne-Task/', // <-- Add this line with your repo name
   plugins: [react()],
 })
